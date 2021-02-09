bcd737d3873dcd291358df4f143aade07d95b957
Naimul Haque <naimulhaque@pop-os.localdomain>
2021-02-09 13:16:25 +0600

    Created chakra theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../styles/theme'
+import Fonts from '../styles/fonts'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+import { theme } from '@chakra-ui/react'
+
+export default {
+  ...theme,
+  fonts: {
+    heading: 'Mulish',
+    body: 'Mulish',
+  },
+  colors: {
+    ...theme.colors,
+    body: '#E5E5E5',
+  },
+}
